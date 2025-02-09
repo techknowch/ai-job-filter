bb361b0ac0ec3ea86ee238b13116488336125f3e
devalinaqvi <79686802+techknowch@users.noreply.github.com>
2025-02-09 22:53:21 +0500

    Update Job-Post component

--- frontend/src/app/components/job-post/job-post.component.ts
+++ frontend/src/app/components/job-post/job-post.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { JobService } from '../../services/job.service';
 
 @Component({
   selector: 'app-job-post',
@@ -8,5 +10,28 @@ import { Component } from '@angular/core';
   styleUrl: './job-post.component.scss'
 })
 export class JobPostComponent {
+  jobForm: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private jobService: JobService
+  ) {
+    this.jobForm = this.fb.group({
+      title: ['', Validators.required],
+      description: ['', Validators.required],
+      company: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.jobForm.valid) {
+      this.jobService.postJob(this.jobForm.value).subscribe(
+        response => {
+          console.log('Job posted successfully', response);
+          this.jobForm.reset();
+        },
+        error => console.error('Error posting job', error)
+      );
+    }
+  }
 }
