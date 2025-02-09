0a0223f0106bb914e0e39ff2a4091e1a0b093b6b
devalinaqvi <79686802+techknowch@users.noreply.github.com>
2025-02-09 22:45:00 +0500

    Create Job Post Component

---
+++ frontend/src/app/components/job-post/job-post.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-job-post',
+  standalone: true,
+  imports: [],
+  templateUrl: './job-post.component.html',
+  styleUrl: './job-post.component.scss'
+})
+export class JobPostComponent {
+
+}
