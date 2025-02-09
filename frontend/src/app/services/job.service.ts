13f1d764943fac64f217d4a83e2665465aa402e7
devalinaqvi <79686802+techknowch@users.noreply.github.com>
2025-02-09 22:52:01 +0500

    Update job.service.ts

--- frontend/src/app/services/job.service.ts
+++ frontend/src/app/services/job.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const API_URL = 'http://localhost:3000/api';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JobService {
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  postJob(jobData: any): Observable<any> {
+    return this.http.post(`${API_URL}/jobs`, jobData);
+  }
+
+  getJobs(): Observable<any> {
+    return this.http.get(`${API_URL}/jobs`);
+  }
+
+  getJobById(id: string): Observable<any> {
+    return this.http.get(`${API_URL}/jobs/${id}`);
+  }
 }
