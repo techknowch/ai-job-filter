6199f4a6e1d11589c627dc5751cd34b47021bcb4
devalinaqvi <79686802+techknowch@users.noreply.github.com>
2025-02-09 22:59:33 +0500

    Add routes frontend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { JobPostComponent } from './components/job-post/job-post.component';
+import { JobListComponent } from './components/job-list/job-list.component';
+import { ResumeUploadComponent } from './components/resume-upload/resume-upload.component';
+import { MatchesComponent } from './components/matches/matches.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/jobs', pathMatch: 'full' },
+    { path: 'jobs', component: JobListComponent },
+    { path: 'post-job', component: JobPostComponent },
+    { path: 'upload-resume', component: ResumeUploadComponent },
+    { path: 'matches', component: MatchesComponent },
+];
