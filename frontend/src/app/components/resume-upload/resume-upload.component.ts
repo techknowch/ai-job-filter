ee407f5c830600407c8369735f2ded7df7fe737c
devalinaqvi <79686802+techknowch@users.noreply.github.com>
2025-02-09 22:46:41 +0500

    Create Job-List, Resume-Upload, Matches components and services Job, and Matching

---
+++ frontend/src/app/components/resume-upload/resume-upload.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resume-upload',
+  standalone: true,
+  imports: [],
+  templateUrl: './resume-upload.component.html',
+  styleUrl: './resume-upload.component.scss'
+})
+export class ResumeUploadComponent {
+
+}
