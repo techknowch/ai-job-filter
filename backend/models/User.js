6d78edcaa8fa3fab61d53574d1a70ae1507971fb
devalinaqvi <79686802+techknowch@users.noreply.github.com>
2025-03-14 23:08:57 +0500

    Update user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,12 +1,34 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    role: { type: String, enum: ['employer', 'jobseeker'], required: true },
-    name: String,
-    company: String,
-    createdAt: { type: Date, default: Date.now }
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['employer', 'jobseeker'],
+        required: true
+    },
+    company: {
+        type: String,
+        required: function () {
+            return this.role === 'employer';
+        }
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
 });
 
-module.exports = mongoose.model('User', userSchema); 
+module.exports = mongoose.model('User', UserSchema); 
