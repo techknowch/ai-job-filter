33892a536376378c91751f52552a864fcce6eb18
devalinaqvi <79686802+techknowch@users.noreply.github.com>
2025-03-14 23:08:17 +0500

    Update Job Model

--- backend/models/Job.js
+++ backend/models/Job.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
 
-const jobSchema = new mongoose.Schema({
+const JobSchema = new mongoose.Schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
     requirements: [String],
-    skills: [String],
+    skills: [{ type: String, required: true }],
     company: { type: String, required: true },
-    location: String,
-    salary: String,
+    location: { type: String, required: true },
+    salary: { type: String },
+    requiredExperience: { type: Number, default: 0 },
     createdAt: { type: Date, default: Date.now },
-    employerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
 });
 
-module.exports = mongoose.model('Job', jobSchema); 
+module.exports = mongoose.model('Job', JobSchema); 
